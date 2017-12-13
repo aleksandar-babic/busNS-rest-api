e9d0dd5ac176710d5719ea8256d1a8a79e0f8ce8
Aleksandar Babic <alekski64@icloud.com>
2017-12-13 12:05:25 +0100

    Added swagger docs

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const port = 8080;
 const busesController = require('./api/controllers/buses');
 const lanesController = require('./api/controllers/lanes');
@@ -16,14 +18,18 @@ if(process.env.NODE_ENV !== 'test') {
     app.use(morgan('combined')); //Apache style logs
 }
 
+//Change swagger host if BASE_HOST env variable is set
+if(process.env.BASE_HOST){
+    swaggerDocument.host = `${process.env.BASE_HOST}:${port}`;
+} else {
+    swaggerDocument.host = `localhost:${port}`;
+}
+
 //Setup body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'}));
-
-//Dummy home page route
-app.get('/', (req, res) => res.json({message: 'Nothing to see here, go for docs!'}));
 
 //Buses routes setup
 app.route('/buses').get(busesController.getBuses);
@@ -31,6 +37,9 @@ app.route('/buses/:id').get(busesController.getBus);
 
 //Lanes routes setup
 app.route('/lanes').get(lanesController.getLanes);
+
+//Swagger UI setup
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port);
 console.log('Listening on port ' + port);
